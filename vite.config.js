a91f65c3e0b313d8d051415d7dc68cfb058a5630
samba ba <ba269962@gmail.com>
2025-01-22 03:41:51 +0000

    installation et configuration de shadcn ui

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
+
 
 export default defineConfig({
     plugins: [
@@ -8,4 +10,9 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'),
+        },
+    },
 });
